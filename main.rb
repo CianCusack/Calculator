df77da9c9b77d4484f14cf138845d6004a1e492c
CianCusack <cianncusack@gmail.com>
2017-09-17 10:13:58 +0100

    'Calculator'

--- main.rb
+++ main.rb
@@ -1,5 +1,9 @@
+require './lib/Calculator'
 calc = Calculator.new()
 
 sum = calc.add(2,2)
+sub = calc.subtract(2,2)
+product = calc.multiply(2,2)
+quotient  = calc.divide(2,2)
 
-puts sum
+puts sum, sub, product, quotient
